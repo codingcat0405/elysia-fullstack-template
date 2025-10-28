b3359225cbd4c726454cbcfa8ce1d05c8b620cb6
lilhuy0405 <luuhuy040501.travail@gmail.com>
2025-10-28 18:24:37 +0700

    complete template

---
+++ src/services/UserService.ts
@@ -0,0 +1,71 @@
+import { User } from "@server/entities/User";
+import { initORM } from "../db";
+import { Elysia } from "elysia";
+import * as jwt from "jsonwebtoken";
+
+export class UserService {
+
+  async register(username: string, password: string): Promise<User> {
+    const db = await initORM()
+    const existUser = await db.user.findOne({ username })
+    if (existUser) {
+      throw new Error("User already exists")
+    }
+    const hashPassword = await Bun.password.hash(password, 'bcrypt')
+    const user = db.user.create({
+      username,
+      password: hashPassword,
+      role: "user"
+    })
+    await db.em.persistAndFlush(user)
+    return user
+  }
+
+  async login(username: string, password: string): Promise<{
+    user: {
+      id: number,
+      username: string,
+      role: string
+    }, jwt: string
+  }> {
+    const db = await initORM()
+    const user = await db.user.findOne({ username })
+    if (!user) {
+      throw new Error("User not found")
+    }
+    const isValid = await Bun.password.verify(password, user.password, 'bcrypt')
+    if (!isValid) {
+      throw new Error("Invalid password")
+    }
+    //generate token
+    const token = jwt.sign({ id: Number(user.id), role: user.role }, process.env.JWT_SECRET ?? "")
+
+    return {
+      user: {
+        id: Number(user.id),
+        username: user.username,
+        role: user.role
+      },
+      jwt: token
+    }
+  }
+
+  async getUserInfo(id: number): Promise<{
+    id: number,
+    username: string,
+    role: string
+  }> {
+    const db = await initORM()
+    const user = await db.user.findOne({ id })
+    if (!user) {
+      throw new Error("User not found")
+    }
+    return {
+      id: user.id,
+      username: user.username,
+      role: user.role
+    }
+  }
+}
+
+export default new Elysia().decorate('userService', new UserService())
